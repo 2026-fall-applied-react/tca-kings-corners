f6edf1c3c0e0304f894cacd141650661ccf249be
bryson-fun <funmaker.bryson@gmail.com>
2026-09-20 20:53:13 -0500

    bunch of cards, but how do they size?? I'm staying tuned...(●__●)

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,76 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-        <div>
-          <h1 className='text-xl font-bold'>
-            Get started
-          </h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-          <button
-            type="button"
-            className="btn btn-lg btn-soft btn-primary"
-            onClick={() => setCount((count) => count + 1)}
-            >
-              Count is {count}
-          </button>
+    <div
+      className='p-3'
+    >
+      <h1 className='text-xl font-bold'>
+        Get started
+      </h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+      </p>
+      <button
+        type="button"
+        className="btn btn-lg btn-soft btn-primary"
+        onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+      </button>
+
+
+      <div className="card w-96 bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Large Card</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
+      </div>
+
+      <div className="card w-96 bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Large Card</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="card w-96 bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Large Card</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="card w-96 bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Large Card</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="card w-96 bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Large Card</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="justify-end card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
   )
 }
