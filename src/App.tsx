d53db8ef340988472ba3ab9209abca19334e94da
bryson-fun <funmaker.bryson@gmail.com>
2026-09-20 21:04:53 -0500

    Daisy UI step component, for potential use...
    Where would I use this tho? hmm.....

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,13 @@ function App() {
         </div>
       </div>
 
+      <ul className="steps steps-vertical">
+        <li className="step step-primary">Register</li>
+        <li className="step step-primary">Choose plan</li>
+        <li className="step">Purchase</li>
+        <li className="step">Receive Product</li>
+      </ul>
+
     </div>
 
   )
