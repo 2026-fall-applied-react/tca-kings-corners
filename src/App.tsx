290b92ceaf6c7129843a78099656b2fb5c169cb1
bryson-fun <funmaker.bryson@gmail.com>
2026-09-23 17:22:37 -0500

    got initial csr working, but components need their own files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import { useState } from 'react'
 import './App.css'
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Router
+} from 'react-router'
+const Home = () => <h1>Home</h1>;
 
-function App() {
-  const [count, setCount] = useState(0)
+const Setup = () => <h1>Setup</h1>;
+
+const Play = () => <h1>Play</h1>;
+
+
+const App = () => {
 
   return (
-        <div>
-          <h1 className='text-xl font-bold'>
-            Get started
-          </h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-          <button
-            type="button"
-            className="btn btn-lg btn-soft btn-primary"
-            onClick={() => setCount((count) => count + 1)}
-            >
-              Count is {count}
-          </button>
+        <div className='p-3'>
+          <HashRouter>
+            <Routes>
+              <Route 
+                path='/'
+                element={
+                  <Home />
+                } />
+
+              <Route 
+                path='/setup'
+                element={
+                  <Setup />
+                } />
+
+              <Route 
+                path='/play'
+                element={
+                  <Play />
+                } />
+
+
+            </Routes>
+          </HashRouter>
         </div>
 
   )
